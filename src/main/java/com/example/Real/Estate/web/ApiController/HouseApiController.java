19b3c233a12a2e54a748699320fd9c8b8389da58
muzbae <muzkh555@gmail.com>
2022-07-29 13:16:50 -0700

    initial commit

--- src/main/java/com/example/Real/Estate/web/ApiController/HouseApiController.java
+++ src/main/java/com/example/Real/Estate/web/ApiController/HouseApiController.java
@@ -4,6 +4,7 @@ package com.example.Real.Estate.web.ApiController;
 import com.example.Real.Estate.Dto.HouseRequestDto;
 import com.example.Real.Estate.Dto.HouseResponseDto;
 import com.example.Real.Estate.Model.House;
+import com.example.Real.Estate.Repository.HouseRepository;
 import com.example.Real.Estate.Service.BookingService2;
 import com.example.Real.Estate.Service.HouseServices;
 import com.example.Real.Estate.web.Api.HouseApi;
@@ -11,10 +12,14 @@ import lombok.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.*;
+import java.io.IOException;
 import java.util.Optional;
 
 @Data
@@ -36,49 +41,24 @@ public class HouseApiController implements HouseApi {
     @Override
     public ResponseEntity createHouse(HouseRequestDto dto) {
 
-        return  ResponseEntity.ok().body(houseServices.addNewHouse(dto));
+        return ResponseEntity.ok().body(houseServices.addNewHouse(dto));
     }
 
     @Override
     public ResponseEntity updateHouse(Long house_id, HouseRequestDto dto) {
-        return  ResponseEntity.ok().body(houseServices.update(house_id,dto));
+        return ResponseEntity.ok().body(houseServices.update(house_id, dto));
     }
 
     @Override
     public ResponseEntity<HouseResponseDto> getHouseById(Long house_id) {
 
-            Optional<House> optionalHouse=houseServices.getHouseById(house_id);
+        Optional<House> optionalHouse = houseServices.getHouseById(house_id);
 
-            if(!optionalHouse.isPresent()){
-                throw new IllegalStateException("House with Id " + house_id+ "Does Not Exist");
-            }
-            House house=optionalHouse.get();
-            HouseResponseDto response=new HouseResponseDto();
-            response.setHouseId(house.getHouseId());
-            response.setBlockNo(house.getBlockNo());
-            response.setFloorNo(house.getFloorNo());
-            response.setHouseNo(house.getHouseNo());
-            response.setSize(house.getSize());
-            response.setType(house.getType());
-            response.setPrice(house.getPrice());
-            response.setAddress(house.getAddress());
-            response.setCity(house.getCity());
-            response.setHouseStatus(house.getHouseStatus());
-            response.setPic(house.getPic());
-            return  ResponseEntity.ok(response);
-
-    }
-
-    @Override
-    public ResponseEntity<HouseResponseDto> deleteHouse(Long house_id) {
-        Optional<House> optionalHouse=houseServices.getHouseById(house_id);
-
-        if(!optionalHouse.isPresent()){
-            throw new IllegalStateException("House with Id " + house_id+ "Does Not Exist");
+        if (!optionalHouse.isPresent()) {
+            throw new IllegalStateException("House with Id " + house_id + "Does Not Exist");
         }
-        houseServices.deleteHouse(house_id);
-        House house=optionalHouse.get();
-        HouseResponseDto response=new HouseResponseDto();
+        House house = optionalHouse.get();
+        HouseResponseDto response = new HouseResponseDto();
         response.setHouseId(house.getHouseId());
         response.setBlockNo(house.getBlockNo());
         response.setFloorNo(house.getFloorNo());
@@ -90,7 +70,32 @@ public class HouseApiController implements HouseApi {
         response.setCity(house.getCity());
         response.setHouseStatus(house.getHouseStatus());
         response.setPic(house.getPic());
-        return  ResponseEntity.ok(response);
+        return ResponseEntity.ok(response);
+
+    }
+
+    @Override
+    public ResponseEntity<HouseResponseDto> deleteHouse(Long house_id) {
+        Optional<House> optionalHouse = houseServices.getHouseById(house_id);
+
+        if (!optionalHouse.isPresent()) {
+            throw new IllegalStateException("House with Id " + house_id + "Does Not Exist");
+        }
+        houseServices.deleteHouse(house_id);
+        House house = optionalHouse.get();
+        HouseResponseDto response = new HouseResponseDto();
+        response.setHouseId(house.getHouseId());
+        response.setBlockNo(house.getBlockNo());
+        response.setFloorNo(house.getFloorNo());
+        response.setHouseNo(house.getHouseNo());
+        response.setSize(house.getSize());
+        response.setType(house.getType());
+        response.setPrice(house.getPrice());
+        response.setAddress(house.getAddress());
+        response.setCity(house.getCity());
+        response.setHouseStatus(house.getHouseStatus());
+        response.setPic(house.getPic());
+        return ResponseEntity.ok(response);
     }
 
     @Override
@@ -98,16 +103,50 @@ public class HouseApiController implements HouseApi {
         return ResponseEntity.ok().body(houseServices.getHouse());
     }
 
-
-//      @Override
-//    public ResponseEntity uploadPicture() {
-//         ResponseEntity <HouseResponseDto> uploadPicture(@RequestParam("") MultipartFile file)
-       //return ResponseEntity.ok().body(houseServices.getHouse());
-       //public House uploadImage(@RequestParam("myFile") MultipartFile file) throws IOException{
-
-          //};
+//
+//    @Autowired
+//    HouseRepository houseRepository;
+//
+//    @PostMapping("upload/image")
+//
+//    public ResponseEntity<HouseResponseDto> uploadImage(@RequestParam("image") MultipartFile file)
+//            throws IOException {
+//        houseRepository.save(Image.builder()
+//                        .name(file.getPic())
+//                        .type(file.getContentType())
+//                        .image(ImageUtility.compressImage(file.getBytes())).build());
+//        return ResponseEntity.status(HttpStatus.OK)
+//                .body(new HouseResponseDto("Image uploaded successfully: " +
+//                        file.getOriginalFilename()));
 //    }
+//
+//    @GetMapping(path = {"/get/image/info/{name}"})
+//    public Image getImageDetails(@PathVariable("name") String name) throws IOException {
+//
+//        final Optional<Image> dbImage = imageRepository.findByName(name);
+//
+//        return Image.builder()
+//                .name(dbImage.get().getName())
+//                .type(dbImage.get().getType())
+//                .image(ImageUtility.decompressImage(dbImage.get().getImage())).build();
+//    }
+//
+//    @GetMapping(path = {"/get/image/{name}"})
+//    public ResponseEntity<byte[]> getImage(@PathVariable("name") String name) throws IOException {
+//
+//        final Optional<Image> dbImage = imageRepository.findByName(name);
+//
+//        return ResponseEntity
+//                .ok()
+//                .contentType(MediaType.valueOf(dbImage.get().getType()))
+//                .body(ImageUtility.decompressImage(dbImage.get().getImage()));
+  //  }
+    }
 
-}
+
+
+
+
+
 
 
